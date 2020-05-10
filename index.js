3f7ac98b60ada0565d87b169a8bd8c7a4ea2bffc
Hamida Sharifova <hamida@Hamidas-MacBook-Pro.local>
2020-05-09 21:08:33 -0700

    sending my lambda precourse material

---
+++ index.js
@@ -0,0 +1,7 @@
+function getInput(){
+    
+    let userInput = document.getElementById("submit").value;            
+             
+    document.write("Thanks for submitting your email! " );
+      
+     }
